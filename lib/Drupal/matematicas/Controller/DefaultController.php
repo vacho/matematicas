15af1dd7cf5af2e9de6a72131444576f152c216b
vacho <vacho@koala-soft.com>
2014-05-10 10:47:42 -0400

    Agregamos operación suma

--- lib/Drupal/matematicas/Controller/DefaultController.php
+++ lib/Drupal/matematicas/Controller/DefaultController.php
@@ -15,4 +15,15 @@ class DefaultController extends ControllerBase
   public function hello($name) {
     return "Hello " . $name . "!";
   }
+
+  /**
+   * suma
+   * @param float $num1
+   * @param float $num2
+   * @return float
+   */
+  public function suma($num1, $num2) {
+    return $num1 + $num2;
+  }
+
 }
